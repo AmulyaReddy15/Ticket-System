3864adbdc34dc118a83c34013e9e5df919f1fcdd
name <pettamaksharavarma@gmail.com>
2026-03-13 22:05:09 +0530

    Initial commit

---
+++ Ticket-Raising-Backendd/src/main/java/com/example/Ticket/Raising/Backendd/TicketRaisingBackenddApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ticket.Raising.Backendd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketRaisingBackenddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketRaisingBackenddApplication.class, args);
+	}
+
+}
